2fa8ef0cb454fbd86e25432c90b79c04bfea3ca4
imRAN-1998 <isharif664@gmail.com>
2020-12-29 21:06:17 +0530

    animation added after screenshot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,8 +26,8 @@ export type Job = {
       transition('* => *' , [
         query(':enter',style({opacity : 0}),{optional : true}),
 
-        query(':enter' , stagger('0ms',[
-          animate('0s ease-in',keyframes([
+        query(':enter' , stagger('200ms',[
+          animate('0.7s ease-in',keyframes([
             style({opacity : 0,transform : 'translateY(-100px)',offset : 0}),
             style({opacity : 0.9,transform : 'translateY(35px)',offset : 0.3}),
             style({opacity : 1,transform : 'translateY(0px)',offset : 1})
